cefbdb2d24933d9dc2ca47cbf604a87975467ec0
Jermany <brown.jermanya@gmail.com@thinkful.com>
2021-05-25 01:16:06 -0400

    some front end work
    Signed-off-by: Jermany <brown.jermanya@gmail.com@thinkful.com>

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,22 +1,25 @@
-const path = require("path");
 
-require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+const path = require('path')
 
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') })
 
-const errorHandler = require("./errors/errorHandler");
-const notFound = require("./errors/notFound");
-const reservationsRouter = require("./reservations/reservations.router");
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
+const errorHandler = require('./errors/errorHandler')
+const notFound = require('./errors/notFound')
+const reservationsRouter = require('./reservations/reservations.router')
+//const tablesRouter = require('./tables/tables.router')
 
-app.use(cors());
-app.use(express.json());
+const app = express()
 
-app.use("/reservations", reservationsRouter);
+app.use(cors())
+app.use(express.json())
 
-app.use(notFound);
-app.use(errorHandler);
+app.use('/reservations', reservationsRouter)
+//app.use('/tables', tablesRouter)
 
-module.exports = app;
+app.use(notFound)
+app.use(errorHandler)
+
+module.exports = app
--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,18 +1,19 @@
+import axios from 'axios'
 /**
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-import formatReservationDate from "./format-reservation-date";
-import formatReservationTime from "./format-reservation-date";
+import formatReservationDate from './format-reservation-date'
+import formatReservationTime from './format-reservation-date'
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000'
 
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
-const headers = new Headers();
-headers.append("Content-Type", "application/json");
+const headers = new Headers()
+headers.append('Content-Type', 'application/json')
 
 /**
  * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
@@ -20,7 +21,7 @@ headers.append("Content-Type", "application/json");
  * This function is NOT exported because it is not needed outside of this file.
  *
  * @param url
- *  the url for the requst.
+ *  the url for the request.
  * @param options
  *  any options for fetch
  * @param onCancel
@@ -31,24 +32,23 @@ headers.append("Content-Type", "application/json");
  */
 async function fetchJson(url, options, onCancel) {
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(url, options)
 
     if (response.status === 204) {
-      return null;
+      return null
     }
 
-    const payload = await response.json();
-
+    const payload = await response.json()
     if (payload.error) {
-      return Promise.reject({ message: payload.error });
+      return Promise.reject({ message: payload.error })
     }
-    return payload.data;
+    return payload.data
   } catch (error) {
-    if (error.name !== "AbortError") {
-      console.error(error.stack);
-      throw error;
+    if (error.name !== 'AbortError') {
+      console.error(error.stack)
+      throw error
     }
-    return Promise.resolve(onCancel);
+    return Promise.resolve(onCancel)
   }
 }
 
@@ -59,11 +59,33 @@ async function fetchJson(url, options, onCancel) {
  */
 
 export async function listReservations(params, signal) {
-  const url = new URL(`${API_BASE_URL}/reservations`);
+  const url = new URL(`${API_BASE_URL}/reservations`)
   Object.entries(params).forEach(([key, value]) =>
     url.searchParams.append(key, value.toString())
-  );
+  )
   return await fetchJson(url, { headers, signal }, [])
     .then(formatReservationDate)
-    .then(formatReservationTime);
+    .then(formatReservationTime)
+}
+
+/**
+ * Retrieves all existing tables.
+ * @returns {Promise<[table]>}
+ *  a promise that resolves to a possibly empty array of table saved in the database.
+ */
+export async function listTables() {
+  const { data } = await axios.get(`${API_BASE_URL}/tables`)
+  return data.data
+}
+/**
+ * Finds reservation by ID
+ * @returns {Promise<[reservation]>}
+ *  a promise that resolves to a possibly empty array of reservation saved in the database.
+ */
+
+export const findReservation = async (reservationId) => {
+  const res = await axios.get(`${API_BASE_URL}/reservations/${reservationId}`, {
+    data: { reservation_id: reservationId },
+  })
+  return res.data.data
 }
