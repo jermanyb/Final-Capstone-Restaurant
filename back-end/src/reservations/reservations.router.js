6ada977b76fd21b7bdce3de05adc28e225c54f08
Jermany <brown.jermanya@gmail.com@thinkful.com>
2021-05-24 20:52:01 -0400

    created tables
    Signed-off-by: Jermany <brown.jermanya@gmail.com@thinkful.com>

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -4,8 +4,9 @@
  * @type {Router}
  */
 
-const router = require("express").Router();
-const controller = require("./reservations.controller");
+const router = require('express').Router()
+const controller = require('./reservations.controller')
+const methodNotAllowed = require('../errors/methodNotAllowed')
 
 router.route("/").get(controller.list);
 
