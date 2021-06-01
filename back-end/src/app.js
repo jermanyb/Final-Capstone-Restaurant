8fff6b6bd46e4479da323d65aebbb5755d2c89d7
Jermany <brown.jermanya@gmail.com@thinkful.com>
2021-05-31 21:21:10 -0400

    update
    Signed-off-by: Jermany <brown.jermanya@gmail.com@thinkful.com>

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const errorHandler = require('./errors/errorHandler')
 const notFound = require('./errors/notFound')
 const reservationsRouter = require('./reservations/reservations.router')
-//const tablesRouter = require('./tables/tables.router')
+const tablesRouter = require('./tables/tables.router')
 
 const app = express()
 
@@ -17,7 +17,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/reservations', reservationsRouter)
-//app.use('/tables', tablesRouter)
+app.use('/tables', tablesRouter)
 
 app.use(notFound)
 app.use(errorHandler)
